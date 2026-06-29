efa34fd6d411f357b9f6d45579739e0d70235602
Akshat Goel <goelakshat2002@gmail.com>
2026-06-29 23:38:43 +0530

    User Register Module

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import session
+from app.database.database import get_db
+from app.models.user import User
 from app.schemas.user import UserRegister
 
 router = APIRouter(
@@ -11,5 +14,17 @@ def test():
     return {"message": "Auth Router Working"}
 
 @router.post("/register")
-def register(user: UserRegister):
-    return user   
+def register(user: UserRegister, db: session = Depends(get_db)):
+    new_user = User(
+        name = user.name,
+        email = user.email,
+        password = user.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return{
+        "message":"New User Registered Successfully",
+        "user_id": new_user.id
+    }   
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 class UserRegister(BaseModel):
-    name: str
-    email: str
-    password: str
+    name: str = Field(min_length=3, max_length=30)
+    email: EmailStr
+    password: str = Field(min_length=3)
